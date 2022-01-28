2eea7a317438818b05d71145d47962667352e59a
Alexys Carrasquillo <alexys.carrasquillo@gmail.com>
2022-01-28 18:47:22 -0500

    seeded db and made sure worked

--- db/schema.sql
+++ db/schema.sql
@@ -6,31 +6,31 @@ USE employees_db;
 
 /* creates table for DEPARTMENT */
 CREATE TABLE department (
-    id INT PRIMARY KEY, 
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
     /* varchar(30) is the character limit and holds department name */
     name VARCHAR(30)
 );
 
 /* creates table for ROLE */
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     /* holds role title*/
     title VARCHAR(30),
     /* holds the salary*/
-    salary DECIMAL
+    salary DECIMAL NULL,
     /* holds reference to the department name is belongs to*/
-    department_id INT
+    department_id INT NULL
 );
 
 /* creates table for EMPLOYEE */
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     /* holds first name*/
     first_name VARCHAR(30),
     /* holds last name*/
     last_name VARCHAR(30),
     /* holds reference to employee role*/
-    role_id INT,
+    role_id INT NULL,
     /* holds reference to manager of current employee*/
-    manager_id INT
+    manager_id INT NULL
 );
