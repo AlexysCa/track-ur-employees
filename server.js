fa02cea2c045d0c8563b5b250acd9b297502a144
Alexys Carrasquillo <alexys.carrasquillo@gmail.com>
2022-01-28 21:45:28 -0500

    created function to add employee

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
+const { type } = require("express/lib/response");
 require("console.table");
 
 // creates connection with mysql
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     port: 3001,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME
-// });
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'favcookie',
+    database: 'employees_db'
+});
 
 // inital prompts uses sees to select for next action 
 function initalPrompt() {
@@ -28,13 +29,74 @@ function initalPrompt() {
             "Update An Employee Role",
             "End"]
     })
-    // .then 
+    .then(function ({ task }) {
+        switch (task) {
+            case "Add An Employee":
+                addEmployee();
+                break;
+        }
+    })
     // need view all department function
     // view all roles function
     // view all employees function
     // add department function
     // add a role function
     // add an employee function
+function addEmployee() {
+    var query = `SELECT role.id, role.title, role.salary
+                FROM role`
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        const roleInput = res.map(({ id, title, salary }) => ({
+            value: id, title: `${title}`, salary: `${salary}`
+        }));
+        console.log(res);
+
+        insertPrompt(roleInput);
+    })
+}
+    
+function insertPrompt(roleInput) {
+    inquirer
+    .prompt([
+    {
+        type: "input",
+        name: "first_name",
+        message: "Enter the employee's first name."
+    },
+    {
+        type: "input",
+        name: "last_name",
+        message: "Enter the employee's last name."
+    },
+    {
+        type: "list",
+        name: "role_id",
+        message: "Select employee's role.",
+        choices: roleInput
+    },
+    ])
+    .then(function (data) {
+        console.log(data);
+
+    var query = `INSERT INTO employee SET ?`
+    connection.query(query,
+        {
+            first_name: data.first_name,
+            last_name: data.last_name,
+            role_id: data.role_id,
+        },
+        function (err, res) {
+            if (err) throw err;
+
+            console.log(res);
+
+            initalPrompt();
+            }
+        )
+    })
+}
     // updates an employee role function 
 }
+
 initalPrompt();
